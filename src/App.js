bedc0b7e6cfa29c0f4d0febc9049f3c4618ef137
JackelineGCP <gessenia_17@hotmail.com>
2018-03-05 04:24:05 -0500

    agregando componente y estilo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+//hexadecimal colors
+//0-9, A B C D E F
+
+let randonColor = ['greenyellow', 'gold', 'red' , 'turquoise',  'royalblue', 'black', 'yellow', 'blue'];
+let color = '#' + Math.random().toString(6).slice(2, 8).toUpperCase();
+
+
+let styleFont = randonColor[Math.floor(Math.random() * 8)];
+const stylesBg = { 
+  backgroundColor: color
+} 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App flex" style={stylesBg}>
+       { <h1 className={styleFont}>Hello I am a colorfull box</h1>}
       </div>
     );
   }
 }
-
 export default App;
+
